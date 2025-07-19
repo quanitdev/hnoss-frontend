4fbaf0f19b80cb8232da8a239d632ee4b4a827eb
DESKTOP-AGUJP0A\Acer <151717860+quanitdev@users.noreply.github.com>
2025-07-20 01:20:39 +0700

    updata BE

--- src/api/axios.jsx
+++ src/api/axios.jsx
@@ -1,38 +1,34 @@
-import axios from 'axios';
+import axios from "axios";
 
-const baseURL = process.env.REACT_APP_API_URL;
-const timeout = +(process.env.REACT_APP_API_TIME_OUT) || 20000;
-
+const baseURL = process.env.REACT_APP_API_URI;
+const timeout = +process.env.REACT_APP_API_TIME_OUT || 20000;
 
 const axiosInstance = axios.create({
-    baseURL,
-    timeout,
+  baseURL,
+  timeout,
 });
 
 axiosInstance.interceptors.request.use(
-    function (config) {
-        config.headers["Content-Type"] = "application/json";
-        config.headers["Access-Control-Allow-Origin"] = "*";
-        return config;
-    },
-    function (error) {
-        return Promise.reject(error);
-    }
+  function (config) {
+    config.headers["Content-Type"] = "application/json";
+    config.headers["Access-Control-Allow-Origin"] = "*";
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
 );
 
 axiosInstance.interceptors.response.use(
-    function (response) {
-        if (response.data) {
-            return response.data;
-        }
-        return response;
-    },
-    function (error) {
-        return Promise.reject(error);
+  function (response) {
+    if (response.data) {
+      return response.data;
     }
+    return response;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
 );
 
 export default axiosInstance;
-
-
-
